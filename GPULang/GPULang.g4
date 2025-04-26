2bfd2197e98f44036c662f65f624fe3acd8f137a
jmanrocks152 <jmanrocks152@gmail.com>
2025-04-26 03:25:40 -0500

    Began implementing AST nodes

--- GPULang/GPULang.g4
+++ GPULang/GPULang.g4
@@ -3,8 +3,8 @@ grammar GPULang;
 program: statement* EOF;
 statement: assignment;
 assignment: ID subscript '=' ID subscript;
-subscript: '[' NUMBER ']';
+subscript: '[' INT ']';
 
 ID: [a-zA-Z_][a-zA-Z_0-9]*;
-NUMBER: [0-9]+;
+INT: [1-9][0-9]*;
 WHITESPACE: [ \r\n\t]+ -> skip;
--- GPULang/src/antlr/GPULangASTNode.cs
+++ GPULang/src/antlr/GPULangASTNode.cs
@@ -1,4 +1,24 @@
+using System.Collections;
+
 namespace GPULang.src.Antlr
 {
-    public interface GPULangASTNode {}
+    public abstract class GPULangASTNode
+    {
+        protected readonly List<GPULangASTNode> _children = [];
+    }
+
+    public class ProgramNode : GPULangASTNode {}
+
+    // TODO: Refactor these constructors with automatic getters and setters as well
+    public class AssignmentNode(string lhsID, string rhsID) : GPULangASTNode
+    {
+        private readonly string _lhsID = lhsID;
+        private readonly string _rhsID = rhsID;
+    }
+
+    public class IntLiteralNode(int value) : GPULangASTNode
+    {
+        private readonly 
+    }
+    
 }
